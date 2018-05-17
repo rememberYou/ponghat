390cbc68e6646462256d0cf9c3fbf3924026be49
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-05-17 19:04:10 +0200

    Add Dockerfile

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except(IOError, ImportError):
 
 setup(
     name="ponghat",
-    version="1.0.0",
+    version="1.0.1",
     description="Pong game using functionalities from the Sense HAT",
     author="Terencio Agozzino",
     author_email="terencio.agozzino@gmail.com",
